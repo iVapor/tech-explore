da9451389c7aff661e53984623625c86fbbb190b
iVapor <yangyy16@outlook.com>
2020-04-17 23:56:33 +0800

    折叠同辈树

--- src/components/SearchTransfer.vue
+++ src/components/SearchTransfer.vue
@@ -1,6 +1,9 @@
 <template>
 <div class="search-transfer">
-    hell
+    <Tree :data="data3"
+          :load-data="loadData"
+          @on-toggle-expand="hideOtherTree"
+          show-checkbox></Tree>
 </div>
 </template>
 
@@ -15,17 +18,22 @@ export default {
     },
     data() {
         return {
-            data3: [
-                {
-                    title: 'parent',
-                    loading: false,
-                    children: []
-                }
-            ],
+            data3: [{
+                id: 11,
+                title: '你是',
+                loading: false,
+                children: []
+            }, {
+                id: 12,
+                title: '被折叠项',
+                loading: false,
+                expand: true,
+                children: []
+            }, ],
         };
     },
     watch: {
-    
+
     },
     created() {
         this.getList()
@@ -34,6 +42,41 @@ export default {
 
     },
     methods: {
+        loadData (item, callback) {
+            log('item', item)
+            setTimeout(() => {
+                let data = [{
+                    id: 0,
+                    title: '我的大女儿',
+                    loading: false,
+                    children: [{
+                        id: 3,
+                        title: '漂亮懂事',
+                        loading: false,
+                        children: []
+                    }, ]
+                }, {
+                    id: 1,
+                    title: '我的大弟',
+                    loading: false,
+                    expand: true,
+                    children: [{
+                        id: 2,
+                        title: '英俊帅气',
+                        loading: false,
+                        children: []
+                    }, ]
+                }, ];
+                if (item.nodeKey === 2) {
+                    data = [{
+                        title: '家乡的云',
+                        loading: false,
+                        children: []
+                    }]
+                }
+                callback(data);
+            }, 100);
+        },
         getList() {
             let server = '/goods-server'
             let url = server + '/goods/category/list'
@@ -50,6 +93,17 @@ export default {
                     log(err);
                 });
         },
+        hideOtherTree(current) {
+            log('current', current)
+            this.foldSibling(current.id)
+        },
+        foldSibling(activeId) {
+            log('activeId', activeId)
+            if (activeId === 11) {
+                this.data3[1].expand = false
+            }
+
+        }
     },
 }
 </script>
