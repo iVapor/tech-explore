c918b28a912ba27bfacfe68036f72df83c5702b7
iVapor <yangyy16@outlook.com>
2020-08-10 17:05:19 +0800

    pass htmltokens1

--- origin/htmlToken/htmltokens1.js
+++ origin/htmlToken/htmltokens1.js
@@ -8,9 +8,148 @@ const ensure = (condition, message) => {
     }
 }
 
+const arrayEquals = (first, second) => {
+    let length = first.length !== second.length
+    if ( length ) {
+        return false
+    }
+
+    for (let i = 0; i < first.length; i++) {
+        let firstEle = first[i]
+        let secondEle = second[i]
+        if (firstEle !== secondEle) {
+            return false
+        }
+    }
+
+    return true
+}
+
+const isNumber = (char) => {
+    let lib = '1234567890'
+    let exist = lib.includes(char)
+    return exist
+}
+
+const isString = (char) => {
+    let lib = 'abcdefghijklmnopqrstuvwxyz'
+    let exist = lib.includes(char)
+    return exist
+}
+
+const isQuote = (char) => {
+    let lib = `'"`
+    let exist = lib.includes(char)
+    return exist
+}
+const isEscape = (char) => {
+    let lib = "\\"
+    let exist = lib.includes(char)
+    return exist
+}
+
+const isSpace = (char) => {
+    return char === ' '
+}
+
+const isSymbol = (char) => {
+    let lib = "{}:,()[]+-*/=<>"
+    let exist = lib.includes(char)
+    return exist
+}
+
+const numberEnd = (s, index) => {
+    let end = index
+    for (let i = index; i < s.length; i++) {
+        let next = s[i + 1]
+
+        if (!isNumber(next)) {
+            log('next', next)
+            end = i
+            break
+        }
+    }
+
+    return end
+}
+
+const stringEnd = (s, index) => {
+    let end = index
+    for (let i = index; i < s.length; i++) {
+        let next = s[i + 1]
+        if (isSpace(next)) {
+            end = i
+            break
+        } else if (isSymbol(next)) {
+            end = i
+            break
+        }
+    }
+
+    return end
+}
+
+const quoteStringEnd = (s, index) => {
+    let end = index
+    for (let i = index; i < s.length; i++) {
+        let current = s[i]
+        if (isEscape(current)) {
+            i++
+        }
+
+        let next = s[i + 1]
+        if (isQuote(next)) {
+            end = i + 1
+            break
+        }
+
+    }
+
+    return end
+}
+
 const htmlTokens = function(htmlString) {
     // htmlString 是 HTML 格式的字符串
     // 解析字符串, 返回对应的 tokens
+
+    let s = htmlString + ' '
+    let i = 0
+    let container = []
+
+    for (;i < s.length;i++) {
+        let a = s[i]
+
+        if (isSpace(a)) {
+
+        } else if (isNumber(a)) {
+            let num = numberEnd(s, i)
+            let cell = s.slice(i, num + 1)
+            let numCell = parseInt(cell)
+
+            container.push(numCell)
+            i = num
+
+        } else if (isString(a)) {
+            let str = stringEnd(s, i)
+            let cell = s.slice(i, str + 1)
+            i = str
+
+            container.push(cell)
+
+        } else if (isQuote(a)) {
+            let q = quoteStringEnd(s, i)
+            let cell = s.slice(i + 1, q)
+            cell = cell.split("\\'").join("'")
+
+            container.push(cell)
+            i = q
+        } else if (isSymbol(a)) {
+            container.push(a)
+        }
+    }
+
+    log('container', container)
+    return container
 }
 
 const testHtmlTokens = function() {
