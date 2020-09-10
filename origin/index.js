10a9771ab09c562f44202528b396ae1a4f0edbcd
iVapor <yangyy16@outlook.com>
2020-09-10 19:59:27 +0800

    完善例子

--- origin/index.js
+++ origin/index.js
@@ -1,28 +1,7 @@
-const log = console.log.bind(console);
 
-const getBranch = (arr, layer) => {
-    return arr.filter(item => item.layer === layer)
+
+const __main = () => {
+    log('in __main')
 }
 
-const handle = (arr, layer) => {
-    let branch = getBranch(arr, layer)
-    if (branch.length === 0) {
-        return []
-    }
-
-    for (let i = 0; i < branch.length; i++) {
-        let eachBranch = branch[i]
-        let sub = getBranch(arr, layer + 1)
-        if (sub.length > 0) {
-
-            let children = handle(arr, layer + 1)
-            let rightSon = children.filter(item => item.pid === eachBranch.id)
-            eachBranch.children = rightSon
-        }
-    }
-
-    return branch
-}
-
-let tree = handle(data.result, 1)
-log('tree', tree)
+__main()
