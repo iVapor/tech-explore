d508773c839d9ab04cee7725cded248e2be9d98a
iVapor <yangyy16@outlook.com>
2022-03-30 17:02:22 +0800

    移动端自适应

---
+++ ant-react/src/components/Header.js
@@ -0,0 +1,13 @@
+import "./header.css"
+
+function Header() {
+    return (
+        <header className="header">
+            <div className={'page-title'}>
+                <a href="//m.aliyun.com" className="icon-home"></a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
--- wechatDemo/app.js
+++ wechatDemo/app.js
@@ -6173,7 +6173,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var config = {"pages":["pages/my/index","pages/index/index"],"window":{"backgroundTextStyle":"light","navigationBarBackgroundColor":"#fff","navigationBarTitleText":"WeChat","navigationBarTextStyle":"black"}};
 _tarojs_runtime__WEBPACK_IMPORTED_MODULE_1__["window"].__taroAppConfig = config
-var inst = App(Object(_tarojs_runtime__WEBPACK_IMPORTED_MODULE_1__["createReactApp"])(_node_modules_babel_loader_lib_index_js_app_js__WEBPACK_IMPORTED_MODULE_3__[/* default */ "a"], react__WEBPACK_IMPORTED_MODULE_4__, react_dom__WEBPACK_IMPORTED_MODULE_5__[/* default */ "a"], config))
+var inst = Header(Object(_tarojs_runtime__WEBPACK_IMPORTED_MODULE_1__["createReactApp"])(_node_modules_babel_loader_lib_index_js_app_js__WEBPACK_IMPORTED_MODULE_3__[/* default */ "a"], react__WEBPACK_IMPORTED_MODULE_4__, react_dom__WEBPACK_IMPORTED_MODULE_5__[/* default */ "a"], config))
 
 Object(_tarojs_taro__WEBPACK_IMPORTED_MODULE_2__["initPxTransform"])({
   designWidth: 750,
